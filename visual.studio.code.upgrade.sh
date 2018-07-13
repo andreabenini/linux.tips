0a27305876c1f35ca54f5683e45e92ed520229ab
Ben <andreabenini@users.noreply.github.com>
2018-07-13 12:56:03 +0200

    code update, few fixes, copyright license
    useful when you're dealing with a distribution that doens't work with .rpm or .deb files.

--- visual.studio.code.upgrade.sh
+++ visual.studio.code.upgrade.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 #
-# Visual Studio Code upgrade procedure
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# @description  Visual Studio Code upgrade procedure
+# @author       Andrea Benini  (andreabenini at google gmail dOt com)
+# @date         2018/06
+#
+# @license      GPLv3
+# @see          History
+#               v1.0 (2018/06)
+#                   Visual Studio Code gets upgraded every 2/3 weeks so I'm really getting
+#                       tired of upgrading it manually, that's the reason of the script.
+#                   rsync and cp to upgrade a generic vscode-stable*64.tar.gz on a generic distro.
+#                   Folks using rpm and debs are probably doing it with their specific upgrade flag
 #
 FILENAME=$1
 if [ "$FILENAME" == "" ]; then
-    echo -e "\nUsage: $0 <vscode.filename.tar.gz>\n"
+    echo -e "\nUsage:\n    $0 <vscode.filename.tar.gz>\n"
+    echo "Possible values:"
+    ls -a code-stable* |xargs -I % echo "    %"
+    echo ""
     exit 1
 fi
 if [ ! -f code ]; then
@@ -13,7 +40,7 @@ if [ ! -f code ]; then
 fi
 
 echo -e "\nThis script will upgrade this Visual Studio Code installation"
-echo -e "with this new upgrade: $FILENAME\n"
+echo -e "with: $FILENAME\n"
 echo -en "Do you want to proceed [y|N] ? "
 read -n 1 CONFIRM
 if [ "$CONFIRM" != "y" ]; then
