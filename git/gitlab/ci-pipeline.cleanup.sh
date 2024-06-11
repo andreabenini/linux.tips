5623ce63c4bec695a94ce4c985eb054e34100750
Ben <andreabenini@users.noreply.github.com>
2024-06-11 23:04:27 +0200

    Keeping $TOKEN outside the script in a secret file

--- git/gitlab/ci-pipeline.cleanup.sh
+++ git/gitlab/ci-pipeline.cleanup.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -e
-# Generate a new one from Settings/Access Tokens and grant user owner or aministrator permissions
-TOKEN="YOUR PRIVATE ACCESS TOKEN HERE"
+# Generate a new one from [Settings/Access Tokens] and grant user owner or aministrator permissions
+#   - Project Settings / Access Tokens
+#   - button [Add new token]
+#       - Token name: "cleanup"
+#       - Role: [Owner]
+#       - Enable checkbox: [api],[read_api],[read_repository],[write_repository]
+#       - Click button [Create Project Access Token]
+#   - Click the eye icon to see the token ID, you won't see it anymore and paste it below
+TOKEN="$(cat token.secret)"
+
+# do not touch this script below this line
+# --------------------------------------------------------------------------------------------------
 # Unique gitlab project ID (Settings/General/Project ID)
 PROJECT_ID="YOUR PROJECT ID"
 # Set your gitlab instance url.
